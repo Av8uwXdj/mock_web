7bc84212d08f869839636aed3f8614e18b1f99d5
Av8uwXdj <develop.s7mvznyb@gmail.com>
2019-11-23 07:43:25 +0900

    init

--- gulp_pug_sass_ts/gulpfile.babel.js
+++ gulp_pug_sass_ts/gulpfile.babel.js
@@ -1,49 +1,75 @@
 import { src, dest, watch, /*series,*/ parallel } from "gulp";
 import pug from "gulp-pug";
 import sass from "gulp-sass";
+import ts from "gulp-typescript";
 import onError from "gulp-notify";
 import plumber from "gulp-plumber";
 import autoprefixer from "gulp-autoprefixer";
 
 // paths
 var paths = {
-    "pug":  "./src/pug/",
-    "sass": "./src/sass/",
-    "ts":   "./src/ts/",
-    "html": "./dst/html/",
-    "css":  "./dst/css/",
-    "js":   "./dst/js"
+    html: {
+        src: "./src/pug/**/!(_)*.pug",
+        dst: "./dst/html/"
+    },
+    css: {
+        src: "./src/sass/**/!(_)*.scss", 
+        dst: "./dst/css/"
+    },
+    js: {
+        src: "./src/ts/**/*.ts",
+        dst: "./dst/js/"
+    }
 }
 
 // options
 var options = {
-    "sass_style": "compressed",
-    "pug_pretty": true
+    sass: {
+        style: "compressed"
+    },
+    pug: {
+        pretty: true
+    }
 }
 
-// task for pug
+// for html
 function task_pug() {
-    return src([paths.pug + "**/*.pug", "!" + paths.pug + "**/_*.pug"])
+    return src(paths.html.src)
     .pipe(plumber({ errorHandler: onError("Error: <%= error.message %>") }))
-    .pipe(pug(options.pug_pretty))
-    .pipe(dest(paths.html));
+    .pipe(pug(options.pug.pretty))
+    .pipe(dest(paths.html.dst));
 }
 
 function watch_pug() {
-    watch([paths.pug + "**/*.pug", "!" + paths.pug + "**/_*.pug"], task_pug);
+    watch(paths.html.src, task_pug);
 }
 
-// task for sass
+// for css
 function task_sass() {
-    return src(paths.sass + "**/*.scss")
+    return src(paths.css.src)
     .pipe(plumber({ errorHandler: onError("Error: <%= error.message %>") }))
     .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(dest(paths.css))
-}
-  
-function watch_sass() {
-    watch(paths.sass + "**/*.scss", task_sass);
+    .pipe(dest(paths.css.dst))
 }
 
-exports.default = parallel(watch_pug, watch_sass);
+function watch_sass() {
+    watch(paths.css.src, task_sass);
+}
+
+// for js
+function task_ts() {
+    return src(paths.js.src)
+    .pipe(ts({ noImplicitAny: true, outFile: 'main.min.js' }))
+    .pipe(dest(paths.js.dst))
+}
+
+function watch_ts() {
+    watch(paths.js.src, task_ts);
+}
+
+exports.default = parallel(
+    watch_pug,
+    watch_sass,
+    watch_ts
+);
---
+++ gulp_pug_sass_ts/src/ts/index.ts
@@ -0,0 +1,8 @@
+class Main{
+    init():void {
+      console.log("Hello Typescript")
+    }
+}
+
+var main = new Main();
+main.init();
